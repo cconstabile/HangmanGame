e8703b7e006cec5e9a5141d678288a7fadcad9d9
cconstabile <christinaconstabile@gmail.com>
2017-02-17 20:05:29 -0800

    updated code

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# HangmanGame
-Homework 3
+HW - {HangmanGame}
+
+Live Link:
+
+
+Description on how to use the app: 
+    Click letters to guess the correct word. You must guess correctly before your lives
+run out or it will mean the game is over!  
+
+
+Technologies Used
+HTML
+CSS
+Javascript
+
+Code Explaination
+As I'm still trying to learn javascript functionality this allowed me to put to use many arrays, for loops, conditions, event listeners and variables to the test. 
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,4 @@
 
-    //start button 
-//     startBtn = function() {
-//       document.getElementById('start').onclick = function() {
-//         showClue.innerHTML = "";
-
-// }
-
     var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
         'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
         'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
@@ -21,8 +14,8 @@
     var counter; // Count correct guesses
     var space; // Number of spaces in word ' '
     var start;
-    var wins = 0;
-    var losses = 0; 
+   // var wins;
+  //  var losses; 
 
     // Get elements
     var showLives = document.getElementById("mylives");
@@ -88,15 +81,16 @@
         showLives.innerHTML = "You have " + lives + " lives";
         if (lives < 1) {
             showLives.innerHTML = "Game Over";
-            losses ++; 
-            losses.innerHTML = "Losses:" + losses;
-        }
+    //        losses.innerHTML = "losses" + 1;
+      //      losses = losses + 1; 
+        }       
+
         for (var i = 0; i < guessedLetters.length; i++) {
             if (counter + space === guessedLetters.length) {
                 showLives.innerHTML = "You Win!";
-                wins ++;
-                wins.innerHTML = "Wins:" + wins;
-            }
+//                document.getElementById("wins");
+  //              win = win + 1;
+                            }
         }
     }
 
@@ -152,20 +146,19 @@
         check();    
     }
 
-    play();
     // Clues
-    hint.onclick = function() {
-        hints = [
-            ["90s Band, originally from Buffalo, NY", "One of the biggest bands of all time", "Made a comeback at the 2006 VMAs", "Piano Player", "King of Pop", "Justin Timberlake's hit of 2016", "Ed Sheeran's first single of 2017", "Greenday's newest realease", "Blink 182's newest album", "Also a city in Italy"],
-            ["Stars Uma Thurman, John Travolta and Samuel L Jackson", "Written by Matt Damon and Ben Affleck", "One of the first movies released in technicolor", "Trilogy based on books by JRR Tolkein", "It has 7 parts and 8 films", "Tom Hanks solves the mysteries of religion", "Stars Robin Williams as a woman", "Starring Jennifer Lawrence and Josh Hutcherson", "A musical set in Revolutionary France", "Set in a Paris opera house"],
-            ["Port city in France", "Declared independence from Austria-Hungary in 1918", "Home of pasta and pizza", "Netherlands capital", "2nd largest city in the US", "1st largest city in the US", "One of Canada's eastern provinces", "Union Jack is their flag", "Has a replica of the Coloseum but is not in Europe", "A city in both Italy and California", "Capital city of England"],
-            ["Layers of meat, sauce and cheese", "originally made in Buffalo", "a staple in the Southern US", "In n Out makes the best", "sushi with avocado", "Green and has a large pit", "Three layered Sandwich", "Vegetables with rice and soy sauce", "a decadent desert", "NY is known for them", "Nestle Tollhouse makes the best"]
-        ];
+    // hint.onclick = function() {
+    //     hints = [
+    //         ["90s Band, originally from Buffalo, NY", "One of the biggest bands of all time", "Made a comeback at the 2006 VMAs", "Piano Player", "King of Pop", "Justin Timberlake's hit of 2016", "Ed Sheeran's first single of 2017", "Greenday's newest realease", "Blink 182's newest album", "Also a city in Italy"],
+    //         ["Stars Uma Thurman, John Travolta and Samuel L Jackson", "Written by Matt Damon and Ben Affleck", "One of the first movies released in technicolor", "Trilogy based on books by JRR Tolkein", "It has 7 parts and 8 films", "Tom Hanks solves the mysteries of religion", "Stars Robin Williams as a woman", "Starring Jennifer Lawrence and Josh Hutcherson", "A musical set in Revolutionary France", "Set in a Paris opera house"],
+    //         ["Port city in France", "Declared independence from Austria-Hungary in 1918", "Home of pasta and pizza", "Netherlands capital", "2nd largest city in the US", "1st largest city in the US", "One of Canada's eastern provinces", "Union Jack is their flag", "Has a replica of the Coloseum but is not in Europe", "A city in both Italy and California", "Capital city of England"],
+    //         ["Layers of meat, sauce and cheese", "originally made in Buffalo", "a staple in the Southern US", "In n Out makes the best", "sushi with avocado", "Green and has a large pit", "Three layered Sandwich", "Vegetables with rice and soy sauce", "a decadent desert", "NY is known for them", "Nestle Tollhouse makes the best"]
+    //     ];
 
-        var categoryIndex = categories.indexOf(chosenCategory);
-        var hintIndex = chosenCategory.indexOf(word);
-        showClue.innerHTML = "Clue: - " + hints[categoryIndex][hintIndex];
-    };
+    //     var categoryIndex = categories.indexOf(chosenCategory);
+    //     var hintIndex = chosenCategory.indexOf(word);
+    //     showClue.innerHTML = "Clue: - " + hints[categoryIndex][hintIndex];
+    // };
 
     // Reset
     document.getElementById('reset').onclick = function() {
@@ -175,5 +168,9 @@
         play(); 
 
         return;
-//}
 }
+
+    document.getElementById('start').onclick = function() {
+        play(); 
+        return;
+	}
