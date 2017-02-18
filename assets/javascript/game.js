6e11c5dc9c1caa5fdf94e743f86a2b9506ba4aaa
cconstabile <christinaconstabile@gmail.com>
2017-02-17 20:35:15 -0800

    updated code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -81,15 +81,15 @@
         showLives.innerHTML = "You have " + lives + " lives";
         if (lives < 1) {
             showLives.innerHTML = "Game Over";
-    //        losses.innerHTML = "losses" + 1;
-      //      losses = losses + 1; 
+        		losses.innerHTML = "losses" + 1;
+      		     losses = losses + 1; 
         }       
 
         for (var i = 0; i < guessedLetters.length; i++) {
             if (counter + space === guessedLetters.length) {
                 showLives.innerHTML = "You Win!";
-//                document.getElementById("wins");
-  //              win = win + 1;
+               document.getElementById("wins");
+               win = win + 1;
                             }
         }
     }
@@ -147,18 +147,18 @@
     }
 
     // Clues
-    // hint.onclick = function() {
-    //     hints = [
-    //         ["90s Band, originally from Buffalo, NY", "One of the biggest bands of all time", "Made a comeback at the 2006 VMAs", "Piano Player", "King of Pop", "Justin Timberlake's hit of 2016", "Ed Sheeran's first single of 2017", "Greenday's newest realease", "Blink 182's newest album", "Also a city in Italy"],
-    //         ["Stars Uma Thurman, John Travolta and Samuel L Jackson", "Written by Matt Damon and Ben Affleck", "One of the first movies released in technicolor", "Trilogy based on books by JRR Tolkein", "It has 7 parts and 8 films", "Tom Hanks solves the mysteries of religion", "Stars Robin Williams as a woman", "Starring Jennifer Lawrence and Josh Hutcherson", "A musical set in Revolutionary France", "Set in a Paris opera house"],
-    //         ["Port city in France", "Declared independence from Austria-Hungary in 1918", "Home of pasta and pizza", "Netherlands capital", "2nd largest city in the US", "1st largest city in the US", "One of Canada's eastern provinces", "Union Jack is their flag", "Has a replica of the Coloseum but is not in Europe", "A city in both Italy and California", "Capital city of England"],
-    //         ["Layers of meat, sauce and cheese", "originally made in Buffalo", "a staple in the Southern US", "In n Out makes the best", "sushi with avocado", "Green and has a large pit", "Three layered Sandwich", "Vegetables with rice and soy sauce", "a decadent desert", "NY is known for them", "Nestle Tollhouse makes the best"]
-    //     ];
+    hint.onclick = function() {
+        hints = [
+            ["90s Band, originally from Buffalo, NY", "One of the biggest bands of all time", "Made a comeback at the 2006 VMAs", "Piano Player", "King of Pop", "Justin Timberlake's hit of 2016", "Ed Sheeran's first single of 2017", "Greenday's newest realease", "Blink 182's newest album", "Also a city in Italy"],
+            ["Stars Uma Thurman, John Travolta and Samuel L Jackson", "Written by Matt Damon and Ben Affleck", "One of the first movies released in technicolor", "Trilogy based on books by JRR Tolkein", "It has 7 parts and 8 films", "Tom Hanks solves the mysteries of religion", "Stars Robin Williams as a woman", "Starring Jennifer Lawrence and Josh Hutcherson", "A musical set in Revolutionary France", "Set in a Paris opera house"],
+            ["Port city in France", "Declared independence from Austria-Hungary in 1918", "Home of pasta and pizza", "Netherlands capital", "2nd largest city in the US", "1st largest city in the US", "One of Canada's eastern provinces", "Union Jack is their flag", "Has a replica of the Coloseum but is not in Europe", "A city in both Italy and California", "Capital city of England"],
+            ["Layers of meat, sauce and cheese", "originally made in Buffalo", "a staple in the Southern US", "In n Out makes the best", "sushi with avocado", "Green and has a large pit", "Three layered Sandwich", "Vegetables with rice and soy sauce", "a decadent desert", "NY is known for them", "Nestle Tollhouse makes the best"]
+        ];
 
-    //     var categoryIndex = categories.indexOf(chosenCategory);
-    //     var hintIndex = chosenCategory.indexOf(word);
-    //     showClue.innerHTML = "Clue: - " + hints[categoryIndex][hintIndex];
-    // };
+        var categoryIndex = categories.indexOf(chosenCategory);
+        var hintIndex = chosenCategory.indexOf(word);
+        showClue.innerHTML = "Clue: - " + hints[categoryIndex][hintIndex];
+    };
 
     // Reset
     document.getElementById('reset').onclick = function() {
